a20aa760a9400c2ebdecc5fdd94bc6c8e70e9622
LucasAlvesCampos <66757359+LucasAlvesCampos@users.noreply.github.com>
2020-10-23 01:23:31 -0300

    Update aplicacao.py

--- aplicacao.py
+++ aplicacao.py
@@ -18,4 +18,6 @@ def jogosdarodada():
 def times():
     return render_template('times.html')
 
-app.run()
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
