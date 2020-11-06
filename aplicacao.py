9399fd139d03367e5adfffd863def32829887b9a
Lucas Alves Campos <lucasalvescampos@gmail.com>
2020-11-06 12:20:15 -0300

    atualização

--- aplicacao.py
+++ aplicacao.py
@@ -1,6 +1,42 @@
 from flask import Flask
-from flask import render_template
+from flask import render_template, request, url_for, redirect
+from flask_sqlalchemy import SQLAlchemy
+
 app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI']='mysql+pymysql://root:mysqlroot@localhost/cadastrotime'
+db = SQLAlchemy(app)
+
+class Cadastrotime(db.Model):
+    __tablename__ = "contatotimes"
+    _id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    nome = db.Column(db.String(50))
+    ano = db.Column(db.String(9))
+    titulos = db.Column(db.String(2))
+    classtemporada = db.Column(db.String(50))
+    
+    def __init__(self, nome, ano, titulos, classtemporada):
+        self.nome = nome
+        self.ano = ano
+        self.titulos = titulos
+        self.classtemporada = classtemporada
+
+class Cadastropartidas(db.Model):
+    __tablename__ = "partidas"
+    _id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    casa = db.Column(db.String(50))
+    visitante = db.Column(db.String(50))
+    data = db.Column(db.String(10))
+    horario = db.Column(db.String(5))
+    
+    
+    def __init__(self, casa, visitante, data, horario):
+        self.casa = casa
+        self.visitante = visitante
+        self.data = data
+        self.horario = horario
+    
+db.create_all()
 
 @app.route('/')
 def index():
@@ -18,4 +54,44 @@ def jogosdarodada():
 def times():
     return render_template('times.html')
 
-app.run()
+@app.route('/cadastro')
+def cadastro():
+    return render_template('cadastro.html')
+
+@app.route('/cadastropartidas')
+def cadastropartidas():
+    return render_template('cadastropartidas.html')
+
+@app.route('/cadastroconcluido')
+def cadastroconcluido():
+    return render_template('cadastroconcluido.html')
+
+@app.route('/cadastrarpartidas', methods=['GET', 'POST'])
+def cadastrarpartidas():
+    if request.method == 'POST':
+        casa = request.form.get('casa')
+        visitante  = request.form.get('visitante')
+        data = request.form.get('data')
+        horario = request.form.get('horario')
+        if casa:
+            f = Cadastropartidas(casa, visitante, data, horario)
+            db.session.add(f)
+            db.session.commit()
+    return redirect(url_for('cadastroconcluido'))
+
+@app.route('/cadastrar', methods=['GET', 'POST'])
+def cadastrar():
+    if request.method == 'POST':
+        nome = request.form.get('nome')
+        ano  = request.form.get('ano')
+        titulos = request.form.get('titulos')
+        classtemporada = request.form.get('classtemporada')
+        if nome:
+            f = Cadastrotime(nome, ano, titulos, classtemporada)
+            db.session.add(f)
+            db.session.commit()
+    return redirect(url_for('cadastroconcluido'))
+    
+
+if __name__ == "__main__":
+    app.run(debug=True)
